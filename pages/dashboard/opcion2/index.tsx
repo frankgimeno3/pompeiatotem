81984032f22f47eb4c7dcc7a560625f8ab90e681
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-06 18:46:33 +0200

    añado contenido opcion2

---
+++ pages/dashboard/opcion2/index.tsx
@@ -0,0 +1,14 @@
+import Navbar from "../../../components/navbar/navbar";
+import Link from 'next/link'
+const opcion2 = () => {
+  return (
+    <>
+    <Navbar />
+        <h1 className="mt-20 text-2xl">
+            Check opcion2
+        </h1>
+    </>
+    );
+};
+
+export default opcion2;
