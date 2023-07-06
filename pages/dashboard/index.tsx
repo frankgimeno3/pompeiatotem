fd6bf8b462d7d43b1aa88fa3092a389013021ba4
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-06 18:45:15 +0200

    instalo types

---
+++ pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+
+import Navbar from "../../components/navbar/navbar";
+import Link from 'next/link'
+const Dashboard = () => {
+  return (
+    <>
+    <Navbar />
+        <h1 className="mt-20 text-2xl">
+            Check home
+        </h1>
+    </>
+    );
+};
+
+export default Dashboard;
