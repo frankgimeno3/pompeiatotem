b638affe1e53b4820a2a942abbc46257217a89f9
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-07 18:34:19 +0200

    creo renderizacion condicional basada en estado

--- pages/landing/es/index.tsx
+++ pages/landing/es/index.tsx
@@ -1,8 +1,18 @@
-import React, { useState, lazy, Suspense } from "react";
-import { useRouter } from "next/router";
-import Link from 'next/link';
+import React, { useState } from "react";
+import Nombre from "../../../components/cuestionario/1nombre/nombre";
+import Conflicto from "../../../components/cuestionario/2conflicto/conflicto";
+import Relaciones from "../../../components/cuestionario/3relaciones/relaciones";
+import Estrategia from "../../../components/cuestionario/4estrategia/estrategia";
+import Resolutividad from "../../../components/cuestionario/5resolutividad/resolutividad";
+import Trabajo from "../../../components/cuestionario/6trabajo/trabajo";
+import Lugar from "../../../components/cuestionario/7lugar/lugar";
+import Humor from "../../../components/cuestionario/8humor/humor";
+import Creatividad from "../../../components/cuestionario/9creatividad/creatividad";
+import Juicio from "../../../components/cuestionario/910juicio/juicio";
+import Horario from "../../../components/cuestionario/911horario/horario";
 
-const Cuestionario: React.FC = () => {
+const Cuestionario = () => {
+  const [componenteactual, setComponenteActual] = useState("nombre");  
   const [nombre, setNombre] = useState("");
   const [conflicto, setConflicto] = useState("");
   const [relaciones, setRelaciones] = useState("");
@@ -14,32 +24,43 @@ const Cuestionario: React.FC = () => {
   const [creatividad, setCreatividad] = useState("");
   const [juicio, setJuicio] = useState("");
   const [horario, setHorario] = useState("");
+  const renderComponenteActual = () => {
 
-  const renderComponent = (stateName: string) => {
-    const Component = lazy(() =>
-      import(`../../components/${stateName}.tsx`)
-    );
-
-    return (
-      <Suspense fallback={<div>Loading...</div>}>
-        <Component />
-      </Suspense>
-    );
+    switch (componenteactual) {
+      case "nombre":
+        return <Nombre />;
+      case "conflicto":
+        return <Conflicto />;
+      case "relaciones":
+        return <Relaciones />;
+      case "estrategia":
+        return <Estrategia />;
+      case "resolutividad":
+        return <Resolutividad />;
+      case "trabajo":
+        return <Trabajo />;
+      case "lugar":
+        return <Lugar />;
+      case "humor":
+        return <Humor />;
+      case "creatividad":
+        return <Creatividad />;
+      case "juicio":
+        return <Juicio />;
+      case "horario":
+        return <Horario />;
+      default:
+        return null; 
+    }
   };
 
   return (
-    <div className="h-screen mx-20">
-      {renderComponent("nombre")}
-      {renderComponent("conflicto")}
-      {renderComponent("relaciones")}
-      {renderComponent("estrategia")}
-      {renderComponent("resolutividad")}
-      {renderComponent("trabajo")}
-      {renderComponent("lugar")}
-      {renderComponent("humor")}
-      {renderComponent("creatividad")}
-      {renderComponent("juicio")}
-      {renderComponent("horario")}
-    </div>
+    <>
+      {renderComponenteActual()}
+    </>
   );
 };
+
+export default Cuestionario;
+
+
