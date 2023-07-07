dedfc1221e06c7ce9f9845a3b79c279248f25922
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-07 17:07:18 +0200

    estructuro base del ES

--- pages/landing/index.tsx
+++ pages/landing/index.tsx
@@ -5,27 +5,31 @@ import Image from "next/image";
 
 const landing = () => {
   return (
-    <div className="h-screen mx-20">
-      <p>TE GUSTARÍA SABER...</p>
-      <h1 className="pt-20 text-2xl mb-10">
+    <div className="h-screen mx-20 pt-6 text-center">
+      <p className="pt-10 text-xs">TE GUSTARÍA SABER...</p>
+      <h1 className="pt-10 text-4xl mb-10">
         ¿QUÉ DIOS DEL OLIMPO TE REPRESENTA?
       </h1>
-      <p>UNA AVENTURA QUE NOS ENSEÑA LAS CUALIDADES DE CADA DIOS ROMANO</p>
+      <p className="text-xs">UNA AVENTURA QUE NOS ENSEÑA LAS CUALIDADES DE CADA DIOS ROMANO</p>
       <Link href="/landing/es">
-        <button className="mt-3 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white">
+        <button className="mt-2 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white">
           JUGAR{" "}
         </button>
       </Link>
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-center mt-10 mb-10">
         <Link href="/landing/en">
-          <div className="flex flex-row">
-            <Image src="../../public/icon/flor.png" alt="flor" />
+          <div className="flex flex-col ">
+            <div className="rounded-full overflow-hidden">
+              <Image src="/icon/flor.png" alt="flor" width={40} height={40} />
+            </div>
             <button> EN</button>
           </div>
         </Link>
         <Link href="/landing/es">
-          <div className="flex flex-row">
-            <Image src="../../public/icon/flor.png" alt="flor" />
+          <div className="flex flex-col">
+            <div className="rounded-full overflow-hidden">
+              <Image src="/icon/flor.png" alt="flor" width={40} height={40} />
+            </div>
             <button> ES</button>
           </div>
         </Link>
