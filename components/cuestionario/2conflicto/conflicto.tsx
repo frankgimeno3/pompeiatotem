b441298de0d6af5c258c27f144e0ad0efe75d195
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-07 19:30:02 +0200

    situacion de conflicto solucionada

--- components/cuestionario/2conflicto/conflicto.tsx
+++ components/cuestionario/2conflicto/conflicto.tsx
@@ -1,25 +1,52 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 
 interface ConflictoProps {
   setComponenteActual: React.Dispatch<React.SetStateAction<string>>;
 }
+
 const Conflicto: React.FC<ConflictoProps> = ({ setComponenteActual }) => {
-  
+  const [selectedOption, setSelectedOption] = useState("");
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+  const handleOptionClick = (option: string) => {
+    setSelectedOption(option);
+    setShowErrorMessage(false);
+  };
+
   const handleSeguirClick = () => {
-    setComponenteActual("relaciones");
+    if (!selectedOption) {
+      setShowErrorMessage(true);
+    } else {
+      setComponenteActual("relaciones");
+    }
   };
 
   return (
     <div className="flex flex-col text-center justify-center">
       <h2>EN UNA SITUACION DE CONFLICTO SOY:</h2>
       <div className="flex flex-row justify-center my-5">
-        <button className="mr-10">PACÍFICO</button>
-        <button className="ml-10">GUERRERO</button>
+        <button
+          className={`mr-10 ${selectedOption === "PACÍFICO" ? "bg-white" : ""}`}
+          onClick={() => handleOptionClick("PACÍFICO")}
+        >
+          PACÍFICO
+        </button>
+        <button
+          className={`ml-10 ${selectedOption === "GUERRERO" ? "bg-white" : ""}`}
+          onClick={() => handleOptionClick("GUERRERO")}
+        >
+          GUERRERO
+        </button>
       </div>
+      {showErrorMessage && (
+        <p className="text-red-500">POR FAVOR, SELECCIONA UNA OPCIÓN ANTES DE SEGUIR</p>
+      )}
       <button
-              className="mt-2 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white"
-              onClick={handleSeguirClick}
-      >SEGUIR</button>
+        className="mt-2 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white"
+        onClick={handleSeguirClick}
+      >
+        SEGUIR
+      </button>
     </div>
   );
 };
