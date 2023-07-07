a2109be621f4716ab9502fc3e54e893f774f75b2
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-07 12:21:11 +0200

    implemento pdf

--- pages/dashboard/success/index.tsx
+++ pages/dashboard/success/index.tsx
@@ -1,17 +1,58 @@
- import Link from 'next/link'
-const success = () => {
+import React from 'react';
+import { useReactToPrint } from 'react-to-print';
+import Link from 'next/link';
+
+const Success = () => {
+  const componentRef = React.useRef<HTMLDivElement>(null); // Especifica el tipo de objeto ref
+
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
   return (
-    <div className='flex flex-col mx-20 justify-center text-center h-screen '>
-         <h1 className="mt-20 text-2xl mb-10">
-            El contenido ha sido enviado satisfactoriamente a impresora!
-        </h1>
-        <div>
-          <Link href="/dashboard">
-          <button className='bg-white mb-10 shadow rounded bg-gray-100 hover:bg-gray-50 px-5 text-xs py-1'> Haga click aquí para terminar</button>
-          </Link>
-        </div>
+    <div className='flex flex-col mx-20 justify-center text-center h-screen'>
+      <h1 className="mt-20 text-2xl mb-10">
+        ¡El contenido ha sido enviado satisfactoriamente a la impresora!
+      </h1>
+      <div>
+        <button
+          className='bg-white mb-10 shadow rounded bg-gray-100 hover:bg-gray-50 px-5 text-xs py-1'
+          onClick={handlePrint}
+        >
+          Imprimir PDF
+        </button>
+        <Link href="/dashboard">
+          <button className='bg-white mb-10 shadow rounded bg-gray-100 hover:bg-gray-50 px-5 text-xs py-1'>
+            Haz clic aquí para terminar
+          </button>
+        </Link>
+      </div>
+      <div style={{ display: 'none' }}>
+        <ComponentToPrint ref={componentRef} />
+      </div>
     </div>
-    );
+  );
 };
 
-export default success;
+const ComponentToPrint = React.forwardRef(function ComponentToPrint(
+  props,
+  ref: React.Ref<HTMLDivElement> // Especifica el tipo de objeto ref
+) {
+  return (
+    <div
+      ref={ref}
+      
+    >
+      <div className='flex flex-col mx-20 justify-center text-center h-screen' >
+        <h1 className="mt-20 text-2xl mb-10">
+          ¡El contenido ha sido enviado satisfactoriamente a la impresora!
+        </h1>
+        <p>Este es un ejemplo de contenido que se convertirá en PDF.</p>
+      </div>
+    </div>
+  );
+});
+
+ComponentToPrint.displayName = 'ComponentToPrint';
+
+export default Success;
