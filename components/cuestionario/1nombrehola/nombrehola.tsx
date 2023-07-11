ee3b83bf5d58df369a39d583afc045fa1f86f281
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-12 01:39:55 +0200

    modifico estilos yahora

--- components/cuestionario/1nombrehola/nombrehola.tsx
+++ components/cuestionario/1nombrehola/nombrehola.tsx
@@ -22,20 +22,22 @@ const Nombrehola: React.FC<NombreholaProps> = ({ setComponenteActual , nombre}) 
 
   return (
     <div className="flex flex-col text-center justify-center">
-      <div className="flex flex-col mb-10">
-        <div className="flex flex-row text-5xl pb-5">
-            <h1>HOLA,</h1>
-            <h2 className="ml-6 text-5xl underline">_{nombre}_</h2>
+      <div className="flex flex-col mb-20 text-center">
+        <div className="flex flex-row text-4xl pb-1 justify-center ">
+            <h1 >HOLA,</h1>
+            <h2 className="ml-6 underline text-3xl ">____{nombre}____</h2>
  
         </div>
-        <p className="text-black">HE AQUÍ TODOS LOS DIOSES ROMANOS</p>
+        <p className="text-black text-xs ">HE AQUÍ TODOS LOS DIOSES ROMANOS</p>
 
       </div>
-
-      <p className="text-black">¿CUÁL DE ELLOS TE REPRESENTA MEJOR?</p>
-      <p className="text-black">
+      <div className="mt-20">
+      <p className="text-black text-xs">¿CUÁL DE ELLOS TE REPRESENTA MEJOR?</p>
+      <p className="text-black text-xs">
         ¡ATRÉVETE A SABER! O COMO DIRÍAN LOS ROMANOS...
       </p>
+      </div>
+
       <button
         className="mt-2 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white"
         onClick={handleSeguirClick}
--- components/cuestionario/1yahora/yahora.tsx
+++ components/cuestionario/1yahora/yahora.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Image from "next/image";
 
 interface YahoraProps {
   setComponenteActual: React.Dispatch<React.SetStateAction<string>>;
@@ -7,15 +8,17 @@ interface YahoraProps {
 const Yahora: React.FC<YahoraProps> = ({ setComponenteActual }) => {
   const [selectedOption, setSelectedOption] = useState("");
   const [showErrorMessage, setShowErrorMessage] = useState(false);
- 
+
   const handleSeguirClick = () => {
-    
-      setComponenteActual("conflicto");
-   };
+    setComponenteActual("conflicto");
+  };
 
   return (
-    <div className="flex flex-col text-center justify-center">
-      <h1>Y AHORA UN PEQUEÑO CUESTIONARIO...</h1>
+    <div className="flex flex-col items-center justify-center text-center">
+      <div className="flex items-center justify-center">
+        <Image src="/romanohablando.png" alt="romano" width={150} height={150} />
+      </div>
+      <h1 className="text-3xl my-3">Y AHORA UN PEQUEÑO CUESTIONARIO...</h1>
       <button
         className="mt-2 bg-white text-gray-800 rounded-full py-2 px-4 border-b-2 border-gray-500 hover:bg-gray-600 hover:text-white"
         onClick={handleSeguirClick}
